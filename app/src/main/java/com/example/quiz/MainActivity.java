854f42ebc1d53fd30e57e7f14fddd7a00e07fac2
student <ghevond03@gmail.com>
2024-07-11 14:52:26 +0400

    updated

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.quiz;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,22 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
-     Question[] questions = new Question[5];
-     int[] b1answers ;
-     int[] b2answers;
-     int[] b4answers;
-     int[] b3answers;
-     int trueanswers;
-     int currentindex ;
+    Question[] questions = new Question[5];
+    int[] b1answers;
+    int[] b2answers;
+    int[] b4answers;
+    int[] b3answers;
+    int trueAnswers;
+    int currentIndex;
 
-        protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            text= findViewById(R.id.text);
+            text = findViewById(R.id.text);
             questions = new Question[]{
                     new Question(getString(R.string.question_Armenia), getString(R.string.answer_Armenia)),
                     new Question(getString(R.string.question_Japan), getString(R.string.answer_Japan)),
@@ -70,138 +68,130 @@ public class MainActivity extends AppCompatActivity {
 
 
             };
-            currentindex =0;
-            trueanswers  = 0;
+            currentIndex = 0;
+            trueAnswers = 0;
             Button a1 = findViewById(R.id.b1);
             Button a2 = findViewById(R.id.b2);
             Button a3 = findViewById(R.id.b3);
             Button a4 = findViewById(R.id.b4);
-            String question = questions[currentindex].getQuestion();
+            String question = questions[currentIndex].getQuestion();
             text.setText(question);
-            a1.setText(b1answers[currentindex]);
-            a2.setText(b2answers[currentindex]);
-            a3.setText(b3answers[currentindex]);
-            a4.setText(b4answers[currentindex]);
+            a1.setText(b1answers[currentIndex]);
+            a2.setText(b2answers[currentIndex]);
+            a3.setText(b3answers[currentIndex]);
+            a4.setText(b4answers[currentIndex]);
             a1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(a1.getText().equals(questions[currentindex].getAnswer())){
-                        trueanswers+=1;
-                        currentindex+=1;
-                    }
-                    else {
-                        currentindex += 1;
+                    if (a1.getText().equals(questions[currentIndex].getAnswer())) {
+                        trueAnswers += 1;
+                        currentIndex += 1;
+                    } else {
+                        currentIndex += 1;
                     }
 
-                    if(currentindex == questions.length){
+                    if (currentIndex == questions.length) {
 
-                        a1.setVisibility(a1.GONE);
-                        a2.setVisibility(a2.GONE);
-                        a3.setVisibility(a3.GONE);
-                        a4.setVisibility(a4.GONE);
-                        text.setText(trueanswers+"/5");
-                    }
-                    else{
+                        a1.setVisibility(View.GONE);
+                        a2.setVisibility(View.GONE);
+                        a3.setVisibility(View.GONE);
+                        a4.setVisibility(View.GONE);
+                        text.setText(trueAnswers + "/5");
+                    } else {
 
                         Question question = questions[0];
                         text.setText(question.getQuestion());
                         updateQuestion();
-                        a1.setText(b1answers[currentindex]);
-                        a2.setText(b2answers[currentindex]);
-                        a3.setText(b3answers[currentindex]);
-                        a4.setText(b4answers[currentindex]);
+                        a1.setText(b1answers[currentIndex]);
+                        a2.setText(b2answers[currentIndex]);
+                        a3.setText(b3answers[currentIndex]);
+                        a4.setText(b4answers[currentIndex]);
                     }
                 }
             });
             a2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(a2.getText().equals(questions[currentindex].getAnswer())){
-                        trueanswers+=1;
-                        currentindex+=1;
-                    }
-                    else {
+                    if (a2.getText().equals(questions[currentIndex].getAnswer())) {
+                        trueAnswers += 1;
+                        currentIndex += 1;
+                    } else {
 
-                        currentindex += 1;
+                        currentIndex += 1;
                     }
 
-                    if(currentindex == questions.length){
+                    if (currentIndex == questions.length) {
 
-                        a1.setVisibility(a1.GONE);
-                        a2.setVisibility(a2.GONE);
-                        a3.setVisibility(a3.GONE);
-                        a4.setVisibility(a4.GONE);
-                        text.setText(trueanswers+"/5");
-                    }
-                    else{
+                        a1.setVisibility(View.GONE);
+                        a2.setVisibility(View.GONE);
+                        a3.setVisibility(View.GONE);
+                        a4.setVisibility(View.GONE);
+                        text.setText(trueAnswers + "/5");
+                    } else {
                         Question question = questions[0];
                         text.setText(question.getQuestion());
                         updateQuestion();
-                        a1.setText(b1answers[currentindex]);
-                        a2.setText(b2answers[currentindex]);
-                        a3.setText(b3answers[currentindex]);
-                        a4.setText(b4answers[currentindex]);
+                        a1.setText(b1answers[currentIndex]);
+                        a2.setText(b2answers[currentIndex]);
+                        a3.setText(b3answers[currentIndex]);
+                        a4.setText(b4answers[currentIndex]);
                     }
                 }
             });
             a3.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(a3.getText().equals(questions[currentindex].getAnswer())){
-                        trueanswers+=1;
+                    if (a3.getText().equals(questions[currentIndex].getAnswer())) {
+                        trueAnswers += 1;
 
-                        currentindex+=1;
-                    }
-                    else {
+                        currentIndex += 1;
+                    } else {
 
-                        currentindex += 1;
+                        currentIndex += 1;
                     }
-                    if(currentindex == questions.length){
+                    if (currentIndex == questions.length) {
 
-                        a1.setVisibility(a1.GONE);
-                        a2.setVisibility(a2.GONE);
-                        a3.setVisibility(a3.GONE);
-                        a4.setVisibility(a4.GONE);
-                        text.setText(trueanswers+"/5");
-                    }
-                    else{
+                        a1.setVisibility(View.GONE);
+                        a2.setVisibility(View.GONE);
+                        a3.setVisibility(View.GONE);
+                        a4.setVisibility(View.GONE);
+                        text.setText(trueAnswers + "/5");
+                    } else {
                         Question question = questions[0];
                         text.setText(question.getQuestion());
                         updateQuestion();
-                        a1.setText(b1answers[currentindex]);
-                        a2.setText(b2answers[currentindex]);
-                        a3.setText(b3answers[currentindex]);
-                        a4.setText(b4answers[currentindex]);
+                        a1.setText(b1answers[currentIndex]);
+                        a2.setText(b2answers[currentIndex]);
+                        a3.setText(b3answers[currentIndex]);
+                        a4.setText(b4answers[currentIndex]);
                     }
                 }
             });
             a4.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(a4.getText().equals(questions[currentindex].getAnswer())){
-                        trueanswers+=1;
+                    if (a4.getText().equals(questions[currentIndex].getAnswer())) {
+                        trueAnswers += 1;
 
-                        currentindex+=1;
-                    }
-                    else {
+                        currentIndex += 1;
+                    } else {
 
-                        currentindex += 1;
+                        currentIndex += 1;
                     }
-                    if(currentindex == questions.length){
-                        a1.setVisibility(a1.GONE);
-                        a2.setVisibility(a2.GONE);
-                        a3.setVisibility(a3.GONE);
-                        a4.setVisibility(a4.GONE);
-                        text.setText(trueanswers+"/5");
-                    }
-                    else{
+                    if (currentIndex == questions.length) {
+                        a1.setVisibility(View.GONE);
+                        a2.setVisibility(View.GONE);
+                        a3.setVisibility(View.GONE);
+                        a4.setVisibility(View.GONE);
+                        text.setText(trueAnswers + "/5");
+                    } else {
                         Question question = questions[0];
                         text.setText(question.getQuestion());
                         updateQuestion();
-                        a1.setText(b1answers[currentindex]);
-                        a2.setText(b2answers[currentindex]);
-                        a3.setText(b3answers[currentindex]);
-                        a4.setText(b4answers[currentindex]);
+                        a1.setText(b1answers[currentIndex]);
+                        a2.setText(b2answers[currentIndex]);
+                        a3.setText(b3answers[currentIndex]);
+                        a4.setText(b4answers[currentIndex]);
                     }
                 }
             });
@@ -209,8 +199,9 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
-    private void updateQuestion(){
-            String question = questions[currentindex].getQuestion();
-            text.setText(question);
+
+    private void updateQuestion() {
+        String question = questions[currentIndex].getQuestion();
+        text.setText(question);
     }
 }
--- app/src/main/java/com/example/quiz/Question.java
+++ app/src/main/java/com/example/quiz/Question.java
@@ -3,7 +3,7 @@ package com.example.quiz;
 public class Question {
     private String question;
     private String answer;
-    private String wronganswer;
+    private String wrongAnswer;
 
     public Question(String question, String answer) {
         this.question = question;
@@ -14,23 +14,14 @@ public class Question {
         return question;
     }
 
-    public void setQuestion(String question) {
-        this.question = question;
-    }
 
     public String getAnswer() {
         return answer;
     }
 
-    public void setAnswer(String answer) {
-        this.answer = answer;
+    public String getWrongAnswer() {
+        return wrongAnswer;
     }
 
-    public String getWronganswer() {
-        return wronganswer;
-    }
 
-    public void setWronganswer(String wronganswer) {
-        this.wronganswer = wronganswer;
-    }
 }
